5fdb431f4e2d7f9981c37904c503740bdf1f5a95
Felipe Matarazzo <felipemps@protonmail.com>
2025-08-21 19:40:07 -0300

    add: multiplexer

--- main.go
+++ main.go
@@ -6,13 +6,18 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", handlerMain)
-
-	fmt.Println("Turning server on port 8080...")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+func handlerMain(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Hello, world!")
 }
 
-func handlerMain(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, world!")
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handlerMain)
+
+	fmt.Println("Turning server on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
